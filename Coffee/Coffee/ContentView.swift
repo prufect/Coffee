72096022eb1419adaa52864ce41ef54308bbd8e3
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-08-22 10:14:43 -0700

    Rewards Screen

--- Coffee/Coffee/ContentView.swift
+++ Coffee/Coffee/ContentView.swift
@@ -10,12 +10,115 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            TitleView()
+                .blur(radius: 20)
+            
+            CardBottomView()
+                .blur(radius: 20)
+            
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z: 10.0))
+            
+            CardView()
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10.0, y: 10.0, z: 10.0))
+            
+            RewardsCardView()
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 20), axis: (x: 10.0, y: 10.0, z: 10.0))
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RewardsCardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Free Drink")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("Rewards Card")
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card Back")
+        }
+        .frame(width: 340, height: 220)
+        .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(10)
+        .shadow(radius: 10)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Rewards")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            .padding(.leading)
+            Image("Illustration5")
+            Spacer()
+        }
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60.0, height: 6)
+                .cornerRadius(3.0)
+                .opacity(0.1)
+            
+            
+            Text("These are all your rewards, be sure to use them at Coffee Cultures to redeem them. Yay :)")
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 30)
+        .offset(y: 600)
     }
 }
