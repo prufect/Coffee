d32140386d520259cd1c2cb3441692fe5e3052b8
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-08-22 09:04:02 -0700

    Import Resources

---
+++ Coffee/Coffee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coffee
+//
+//  Created by Prudhvi Gadiraju on 8/22/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
